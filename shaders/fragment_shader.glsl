57bcecbc69f225c9382d033bdfb7758c229601cb
Quinton Grant <qqgrant@gmail.com>
2024-12-17 16:28:36 -0700

    Removed Unnecessary  Files

--- shaders/fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec3 v_Color;          // Input color from vertex shader
-out vec4 frag_Color;      // Output color to the framebuffer
-
-void main()
-{
-    frag_Color = vec4(v_Color, 1.0); // Set the final fragment color
-}
